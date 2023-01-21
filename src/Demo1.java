20c23e3060c39fae55b9c8b356255c331dee009e
aanchalatre2014 <aanchalatre2014@gmail.com>
2023-01-21 16:22:21 +0530

    Source Code

---
+++ src/Demo1.java
@@ -0,0 +1,7 @@
+public class Demo1 {
+    public static void main(String[]args){
+        System.out.println("Hello beautiful world");
+        System.out.println("How have you been");
+        System.out.println("Have a peaceful journey");
+    }
+}
